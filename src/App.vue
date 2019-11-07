500a83a91a8fb022ba899f44ffefc0d0e4fbde74
Shunya <yamasyunizig1216@gmail.com>
2019-11-08 00:07:14 +0900

    side作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div>
+  <div class="app">
+
+    <!-- サイドバーの中身 -->
+    <v-navigation-drawer v-model="sideNav" absolute left temporary>
+      <v-list-item>
+        <v-icon class="face">mdi-face</v-icon>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            UserName
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+
+    </v-navigation-drawer>
+    
+
+    <!-- ここからメインのバー -->
     <v-toolbar>
-      <v-toolbar-title>Vue-Meetup</v-toolbar-title>
+      <v-app-bar-nav-icon @click.native.stop="sideNav = !sideNav"></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-toolbar-items>
-      <v-btn text>
-        <v-icon left>mdi-face</v-icon>
-         test
-      </v-btn>
-    </v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+      
     </v-toolbar>
-
-
-
     
   </div>
 </template>
 
 <script>
+
+
 export default {
-  data: () => ({
-    //
-  }),
+  name: 'App',
+
+  components: {
+    
+  },
+
+  data(){
+    return{
+      sideNav: false,
+    }
+  }
 };
 </script>
+
+<style>
+.face{
+  margin-right: 10px
+}
+</style>
