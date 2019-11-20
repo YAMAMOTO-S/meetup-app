e36cb1e533f5cbf998f9c60c51263931cfff309c
Shunya <yamasyunizig1216@gmail.com>
2019-11-20 16:01:42 +0900

    Signupページを作成してpassword確認methodの作成

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,5 +1,68 @@
 <template>
-   <div>
-      <p>Sign up Page is here !!</p>
-   </div>
+<v-container>
+      <v-row>
+
+         <v-col class="mx-auto" cols="12" md="9" sm="9">
+            <form @submit.prevent="onSignup">
+               <h2 class="title">Create your Account!!</h2>
+               <br>
+
+               <v-text-field label="Email" name="email" id="email" required v-model="email" type="email">
+               </v-text-field>
+
+               <v-text-field label="Password" name="password" id="password" required v-model="password" type="password">
+               </v-text-field>
+
+<!-- 固有のルールを設定してPASSWORDの確認を行う -->
+               <v-text-field label="Confirm Password" name="confirmPassword" id="confirmPassword" v-model="confirmPassword" type="password"
+                  :rules="[comparePassword]">
+               </v-text-field>
+
+               <br>
+               <div  class="subbtn">
+<!-- 全て入れないとボタンが浮き出ないようにしている。 -->
+                  <v-btn type="submit">Create</v-btn>
+               </div>
+
+            </form>
+         </v-col>
+
+      </v-row>
+   </v-container>
 </template>
+
+<script>
+export default {
+   data(){
+      return{
+         email: '',
+         password: '',
+         confirmPassword: ''
+      }
+   },
+   computed: {
+// PASSWORD確認
+      comparePassword(){
+         return this.password !== this.confirmPassword ? 'Psswords do not match' : ''
+      }
+   },
+   methods: {
+      onSignup(){
+         console.log({email: this.email, password: this.password, confirmPassword: this.confirmPassword})
+      }
+   }
+}
+</script>
+
+
+<style scoped>
+.title{
+   color: black;
+   text-align: center;
+   margin: 50px;
+}
+.subbtn{
+   text-align: center;
+   margin: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import {store} from './store'
 import vuetify from './plugins/vuetify';
 import DateFilter from './filters/date'
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -13,5 +14,15 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+
+    created() {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyD7d9R7giakKKiciHbywU7U093cxQv2SRM',
+        authDomain: 'any-meetup.firebaseapp.com',
+        databaseURL: 'https://any-meetup.firebaseio.com',
+        projectId: 'any-meetup',
+        storageBucket: 'any-meetup.appspot.com',
+      })
+    }
 }).$mount('#app')
